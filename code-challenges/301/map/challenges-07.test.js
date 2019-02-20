0ec005373288248e1092829a26d31946f891ecae
HCoggers <harry.cogswell@gmail.com>
2019-02-19 18:58:07 -0800

    completes challenge 3

--- code-challenges/301/map/challenges-07.test.js
+++ code-challenges/301/map/challenges-07.test.js
@@ -37,7 +37,7 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 ------------------------------------------------------------------------------------------------ */
 
 const mapTwoToThe = (arr) => {
-  // Solution code here...
+  return arr.map(v => Math.pow(2, v));
 }
 
 /* ------------------------------------------------------------------------------------------------
