c8afa67de78e64eb3561db9e153abf2aee5c2550
HCoggers <harry.cogswell@gmail.com>
2019-02-21 08:25:44 -0800

    completes challenge 6

--- code-challenges/301/filter/challenges-08.test.js
+++ code-challenges/301/filter/challenges-08.test.js
@@ -146,7 +146,7 @@ const characters = [
 ];
 
 const getCharactersWithoutChildren = (arr) => {
-  // Solution code here...
+  return arr.filter(val => !(val.children));
 };
 
 /* ------------------------------------------------------------------------------------------------
