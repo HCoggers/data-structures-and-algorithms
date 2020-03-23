4c0218b63fb14e51015e488602e3ba2a4e65575d
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-23 11:17:26 -0700

    completes method comments

--- code-challenges/401/LinkedList/LinkedList/Classes/LinkList.cs
+++ code-challenges/401/LinkedList/LinkedList/Classes/LinkList.cs
@@ -6,14 +6,26 @@ namespace LinkedList.Classes
 {
     public class LinkList
     {
+        /// <summary>
+        /// The First Node in a linked list. The entry point to traverse the list
+        /// </summary>
         public Node Head { get; set; }
 
+        /// <summary>
+        /// Inserts a new node at the beginning of a linked list, assigning it to head, and moving the previous head down to list.
+        /// </summary>
+        /// <param name="newNode">The node to be inserted</param>
         public void Insert(Node newNode)
         {
             newNode.Next = this.Head;
             this.Head = newNode;
         }
         
+        /// <summary>
+        /// Checks if a given Value is represented anywhere in the linked list's nodes. returns true if it finds a matching value.
+        /// </summary>
+        /// <param name="searchValue">The value to search for</param>
+        /// <returns>True if found. False if not</returns>
         public bool Includes(int searchValue)
         {
             Node current = this.Head;
@@ -27,6 +39,11 @@ namespace LinkedList.Classes
             return includes;
         }
 
+        /// <summary>
+        /// Returns a formatted string of all the values of nodes in your list.
+        /// Format: "{ a } -> { b } -> { c } -> NULL"
+        /// </summary>
+        /// <returns>string representation of LinkList</returns>
         public override string ToString()
         {
             string collection = "";
