342a3eccba8be62720df017bddc327a09d751617
Harry Cogswell <harry.cogswell@gmail.com>
2020-04-08 12:18:25 -0700

    completes breadth challenge, still need to fix preorder method

--- code-challenges/401/Tree/Tree/Classes/Node.cs
+++ code-challenges/401/Tree/Tree/Classes/Node.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Tree.Classes
+namespace LinkedList.Classes
 {
     public class Node
     {
         public int Value { get; set; }
+        public Node Next { get; set; }
         public Node LeftChild { get; set; }
         public Node RightChild { get; set; }
+
     }
 }
